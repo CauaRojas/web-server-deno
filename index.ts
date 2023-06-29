0e6b2db5fc996a4b2f504de19d8acb806adf5b3d
Cauã Alencar Rojas Romero <cauarojas@hotmail.com>
2023-06-28 23:20:03 -0300

    started the process of cleaning the url string

--- index.ts
+++ index.ts
@@ -1,6 +1,14 @@
 import { serve } from 'https://deno.land/std@0.192.0/http/server.ts'
 
-async function handler(_req: Request) {
+const removeBaseUrl = (url: string) =>
+    url
+        .split('/')
+        .filter((_, index) => index > 2)
+        .join('')
+
+async function handler(req: Request) {
+    console.log(removeBaseUrl(req.url))
+
     return new Response('Hello World!')
 }
 serve(handler, { port: 5000 })
